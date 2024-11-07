6a2c0641e478e3aab662de7d11c15bc7a78ed57a
Adam Miller <adammiller.2000nj@gmail.com>
2024-11-06 19:02:11 -0500

    added variables to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react';
 import './App.css'
+import Background from './Components/Background/Background';
 
 function App() {
 
-  return (
-    <>
+  let heroData = [
+    {
+      text1: "Dive into",
+      text2: "your skills"
+    },
+    {
+      text1: "Indulge",
+      text2: "your passions"
+    },
+    {
+      text1: "Give in to",
+      text2: "your creativity"
+    }
+  ];
 
-    </>
-  )
-}
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+    </div>
+  );
+};
 
 export default App;
