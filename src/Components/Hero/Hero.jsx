2985eb306b3197b3cb91d4202a2a577032a4b053
Adam Miller <adammiller.2000nj@gmail.com>
2024-11-06 19:32:02 -0500

    added html elements to Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,43 @@
 import './Hero.css';
+import arrow_btn from '../../assets/arrow_btn.png';
+import play_icon from '../../assets/play_icon.png';
+import pause_icon from '../../assets/pause_icon.png';
 
-export default function Hero() {
+export default function Hero({ heroData, setHeroCount, heroCount, setPlayStatus, playStatus }) {
 
     return (
-        <div>
-
+        <div className='hero'>
+            <div className="hero-text">
+                <p>{heroData.text1}</p>
+                <p>{heroData.text2}</p>
+            </div>
+            <div className="hero-explore">
+                <p>Explore the features</p>
+                <img src={arrow_btn} alt="" />
+            </div>
+            <div className="hero-dot-play">
+                <ul className="hero-dots">
+                    <li
+                        className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}
+                        onClick={() => setHeroCount(0)}
+                    >
+                    </li>
+                    <li
+                        className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}
+                        onClick={() => setHeroCount(1)}
+                    >
+                    </li>
+                    <li
+                        className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
+                        onClick={() => setHeroCount(2)}
+                    >
+                    </li>
+                </ul>
+                <div className="hero-play">
+                    <img onClick={() => setPlayStatus(!playStatus)} src={playStatus ? pause_icon : play_icon} alt="" />
+                    <p>See the video!</p>
+                </div>
+            </div>
         </div>
     );
 };
