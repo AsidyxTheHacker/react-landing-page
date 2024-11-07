498d025c89418526be377a7c2e3ebe5284935e4b
Adam Miller <adammiller.2000nj@gmail.com>
2024-11-06 19:06:11 -0500

    set background variables for Background.jsx

--- src/Components/Background/Background.jsx
+++ src/Components/Background/Background.jsx
@@ -1,10 +1,28 @@
 import './Background.css';
+import video from '../../assets/video1.mp4';
+import image1 from '../../assets/image1.png';
+import image2 from '../../assets/image2.png';
+import image3 from '../../assets/image3.png';
 
-export default function Background() {
+export default function Background({ playStatus, heroCount }) {
 
-    return (
-        <div>
-
-        </div>
-    );
+    if (playStatus) {
+        return (
+            <video className='background' autoPlay loop muted>
+                <source src={video} type='video/mp4' />
+            </video>
+        );
+    } else if (heroCount === 0) {
+        return (
+            <img src={image1} className='background' alt="" />
+        );
+    } else if (heroCount === 1) {
+        return (
+            <img src={image2} className='background' alt="" />
+        );
+    } else if (heroCount === 2) {
+        return (
+            <img src={image3} className='background' alt="" />
+        );
+    };
 };
